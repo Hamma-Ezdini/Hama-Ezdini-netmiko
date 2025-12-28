8a13e13738dd6f1e52711abfd97665d462aadd63
Hamma-Ezdini <mh.ezdini@gmail.com>
2025-12-28 21:00:46 +0100

    script acce au routeur cisco

--- main.py
+++ main.py
@@ -3,4 +3,41 @@ def dire_salut():
     print("Salut, Git!")
 
 dire_salut()
+print("Hello, Git!")
+from netmiko import ConnectHandler
+
+device = {
+    "device_type": "cisco_ios",
+    "host": "sandbox-iosxr-1.cisco.com",
+    "username": "admin",
+    "password": "C1sco12345",
+    "port": 22,
+}
+
+def main():
+    connexion = ConnectHandler(**device)
+
+    # 1) show clock
+    print("=== DATE DU ROUTEUR ===")
+    print(connexion.send_command("show clock"))
+
+    # 2) Save interfaces
+    print("\n=== SAUVEGARDE DES INTERFACES ===")
+    interfaces = connexion.send_command("show ip interface brief")
+    print(f"\n {interfaces}")
+    with open("interfaces.txt", "w") as f:
+        f.write(interfaces)
+    print("Interfaces enregistrées dans interfaces.txt")
+
+if __name__ == "__main__":
+    main()
+
+
+
+print("Hello, Git!")
+
+def dire_salut():
+    print("Salut, Git!")
+
+dire_salut()
 
